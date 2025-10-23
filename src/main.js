c0232b9bb663f3853cc299ed74f9caa0c00d19a4
Ivan Gurulev <ivangurulev6043@gmail.com>
2025-10-23 18:37:44 +0300

    Added the Vite to the project. Compressed all images to avif format.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import axios from 'axios'
+import 'bulma/css/bulma.min.css';
+import './fonts/fonts.css';
+import './styles/style.scss';
+
 const root = document.querySelector('#houses');
 const accordionTemplate = document.querySelector('#accordion-template').content;
 
@@ -13,7 +18,7 @@ axios.get('https://wizard-world-api.herokuapp.com/Houses')
             accordionElement.querySelector('.house-founder').textContent = `Основатель - ${house.founder}`;
             accordionElement.querySelector('.house-ghost').textContent = `Призрак - ${house.ghost}`;
             accordionElement.querySelector('.house-animal').textContent = `Символ - ${house.animal}`;
-            accordionElement.querySelector('.house-image').setAttribute('src', `images/${house.name}.png`)
+            accordionElement.querySelector('.house-image').setAttribute('src', `/images/${house.name}.avif`)
             root.appendChild(accordionElement);
         })
      });
